de11e36f7dfcd2900c6dbc26ab6686475483e2d1
Mono <125167202+Mono0087@users.noreply.github.com>
2023-09-10 21:55:01 +0500

    Add AI for second player

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const gameBoard = function () {
-    let board = ['', '', '', '', '', '', '', '', '', ''];
+    let board = ['', '', '', '', '', '', '', '', ''];
     const players = [{ name: 'Player1', mark: 'o', type: 'player' }, { name: 'Player2', mark: 'x', type: 'player' }];
     const gameState = {
         currentPlayer: players[0].name,
@@ -7,7 +7,6 @@ const gameBoard = function () {
         isOver: false,
         turnCount: 0,
     };
-
     //Cache DOM
     const gameDOM = document.querySelector('.game_module');
     const boardEl = gameDOM.querySelector('.field_container');
@@ -30,7 +29,6 @@ const gameBoard = function () {
                 boardEl.children[i].classList.add('marked_x');
             } else if (val === 'o') {
                 boardEl.children[i].classList.add('marked_circle');
-            } else {
             }
         })
     }
@@ -45,8 +43,8 @@ const gameBoard = function () {
         if (gameState.currentPlayer === players[0].name) {
             gameState.currentPlayer = players[1].name;
             gameState.currentMark = players[1].mark;
-            if (players[1].type === 'AI') {
-                _computerChoice();
+            if (_getCurrentPlayerType() === 'AI') {
+                _computerChoice(gameState.currentMark);
             }
         } else {
             gameState.currentPlayer = players[0].name;
@@ -69,31 +67,93 @@ const gameBoard = function () {
         gameState.isOver = true;
         resultSpan.textContent = result;
     }
-    function _computerChoice() {
-        for (let i = 0; i < 1;) {
-            let index = Math.floor(Math.random() * 9);
-            if (!board[index]) {
-                addMarkToBoard(index);
-                ++i;
+    function _getCurrentPlayerType() {
+        let obj = players.find(p => p.name === gameState.currentPlayer);
+        return obj.type;
+    }
+    function _computerChoice(mark) {
+        let oppositeMark;
+        if (mark === 'x') {
+            oppositeMark = 'o';
+        } else {
+            oppositeMark = 'x';
+        }
+
+        function minimax(position, maximizingPlayer) {
+            let remainingFields = function () {
+                let n = 0;
+                position.forEach(field => {
+                    if (!field) {
+                        ++n;
+                    }
+                })
+                return n;
+            }
+            if (_victoryCheck(position)) {
+                if (maximizingPlayer) {
+                    return 1 * (remainingFields() + 1);
+                } else {
+                    return -1 * (remainingFields() + 1);
+                }
+            } else if (!position.includes('')) {
+                return 0;
+            }
+            if (maximizingPlayer) {
+                let maxEval = -Infinity;
+                for (let i = 0; i < board.length; ++i) {
+                    if (!board[i]) {
+                        board[i] = oppositeMark;
+                        let eval = minimax(board, false);
+                        maxEval = Math.max(maxEval, eval);
+                        board[i] = '';
+                    }
+                }
+                return maxEval;
             } else {
-                if (gameState.isOver) ++i;
+                let minEval = +Infinity;
+                for (let i = 0; i < board.length; ++i) {
+                    if (!board[i]) {
+                        board[i] = mark;
+                        let eval = minimax(board, true);
+                        minEval = Math.min(minEval, eval);
+                        board[i] = '';
+                    }
+                }
+                return minEval;
             }
         }
+        let bestMove;
+        let bestScore = -Infinity;
+        for (let i = 0; i < board.length; ++i) {
+            if (!board[i]) {
+                board[i] = gameState.currentMark;
+                move = board;
+                currentScore = minimax(move, true);
+                if (currentScore > bestScore) {
+                    bestScore = currentScore;
+                    bestMove = i;
+                }
+                console.log(currentScore, i)
+                board[i] = '';
+            }
+        }
+        addMarkToBoard(bestMove)
     }
+
     //Public methods
     function addMarkToBoard(i) {
         if (!board[i] && i <= 8 && i >= 0 && !gameState.isOver) {
             board[i] = gameState.currentMark;
-            _renderBoard();
             let isVictory = _victoryCheck(board);
             if (isVictory) {
                 _endGame(gameState.currentPlayer);
             } else if (!isVictory && gameState.turnCount === 8) _endGame('Tie');
             _nextPlayerTurn();
+            _renderBoard();
         };
     }
     function restart() {
-        board = ['', '', '', '', '', '', '', '', '', ''];
+        board = ['', '', '', '', '', '', '', '', ''];
         _renderBoard();
         gameState.currentPlayer = players[0].name;
         gameState.currentMark = players[0].mark;
